7025071e67bd854ac6ce53207f09af73fa0b7916
Hitsubato <fabricio.v.o@hotmail.com>
2020-02-23 00:28:12 -0300

    Update Inventory.js

--- src/inventory/Inventory.js
+++ src/inventory/Inventory.js
@@ -1,6 +1,6 @@
 
 import Item from '../prefabs/items/Item'
-import Potion from '../Inventory/potion';
+import Potion from './Potion';
 import ItemMenuItem from '../prefabs/HUD/ItemMenuItem';
 import firebase from 'firebase/app';
 import auth from 'firebase/auth';
