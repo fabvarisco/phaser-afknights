df2787d4f515e7cfd4d49a3f059c180a5e037fbf
Fabricio Varisco <fabricio.v.o@hotmail.com>
2022-01-09 00:30:04 -0300

    Ajustes do webpack

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,11 +1,9 @@
 import Prefab from '../prefabs/Prefab';
 import JSONLevelScene from "./JSONLevelScene";
-import Unit from '../prefabs/Unit/Unit';
 import MenuItem from '../prefabs/HUD/MenuItem'
 import PhysicalAttackMenuItem from '../prefabs/HUD/PhysicalAttackMenuItem'
 import MagicalAttackMenuItem from '../prefabs/HUD/MagicalAttackMenuItem'
 import Menu from '../prefabs/HUD/Menu'
-import PriorityQueue from '../priority-queue.min.js';
 import PlayerUnit from '../prefabs/Unit/PlayerUnit';
 import EnemyUnit from '../prefabs/Unit/EnemyUnit';
 import EnemyMenuItem from '../prefabs/HUD/EnemyMenuItem';
@@ -16,6 +14,8 @@ import ItemMenuItem from '../prefabs/HUD/ItemMenuItem.js';
 
 import PlayerData from '../prefabs/playerData';
 import BackMenuItem from '../prefabs/HUD/backMenuItem';
+
+import PriorityQueue from 'js-priority-queue';
 
 class GameScene extends JSONLevelScene {
 
