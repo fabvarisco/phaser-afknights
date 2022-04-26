2284d8285abdc7b8ab09310b56a0f7a81801b61a
Fabricio Oliveira <fabricio.v.o@hotmail.com>
2022-04-25 21:24:23 -0300

    build working

--- webpack/base.js
+++ webpack/base.js
@@ -4,26 +4,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+    
   mode: "development",
   devtool: "eval-source-map",
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
+    rules: [ 
+        { 
+           test: /\.js$/, // checks for files with .js extension in // the path specified below
+           include: path.resolve(__dirname, "src/"), // checks in // this path
+           exclude: /node_modules/, // exclude node_modules folder
+           use: { 
+              loader: "babel-loader",
+              options: { 
+                 presets: ["env"],
+              }, // uses babel-loader to transpile your ES6 code
+           },
+        },
+        { 
+           test: [/\.vert$/, /\.frag$/],
+           use: "raw-loader",
+        }, // in case you need to use Vertex and Fragment shaders, // this loader will bundle them for you.
+        { 
+           test: /\.(gif|png|jpe?g|svg|xml)$/i,
+           use: "file-loader",
+        }, // in case you need to use images, this loader will    // bundle them for you
+     ],
   },
   plugins: [
     new CleanWebpackPlugin({
@@ -36,5 +41,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html"
     })
-  ]
+  ],
 };
